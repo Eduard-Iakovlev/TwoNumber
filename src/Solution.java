1b3876e83149a9748b5abc63374be11bb59477d3
Eduard Iakovlev <yakovlevadn@mail.ru>
2024-08-19 11:33:35 +0700

    Использовал рстаток от деления.

--- src/Solution.java
+++ src/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int over = 0;
 
         while(l1 != null || l2 != null){
-            current.val = over;
+            
             if(l1 != null){
                 current.val += l1.val;
             }
@@ -13,13 +13,13 @@ public class Solution {
                 current.val += l2.val;
             }
             if(current.val > 9){
-                current.val -= 10;
+                current.val %= 10;
                 over = 1;
             }else over = 0;
             if(l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
             if(l1 != null || l2 != null){
-                current.next = new ListNode();
+                current.next = new ListNode(over);
                 current = current.next;
             }
         }
